2fa45b3bbce66a7023c044a1509027cda5ff0608
briangweber <briangweber@users.noreply.github.com>
2022-12-12 22:39:57 -0500

    Read metadata from filename if headers not present, add ability to add notes for summary (#2)

---
+++ src/filemetadata.ts
@@ -0,0 +1,53 @@
+import _ from "lodash";
+import path from "path";
+import { getNumericKeyword, readKeywordsFromFile } from "./fits/keywordreader";
+import { parseNumber } from "./helpers";
+
+export type FileMetadata = {
+  exposureTime?: number;
+  temperature?: number;
+  filter?: string;
+  date?: string;
+	gain?: number;
+	offset?: number;
+}
+
+export function getMetadataFromFile(filePath: string): FileMetadata {
+	const fileName = path.basename(filePath);
+  
+	// get FITS keywords
+	const keywords = readKeywordsFromFile(filePath);
+
+	const keywordMetadata = {
+		exposureTime: getNumericKeyword("EXPTIME", keywords, true),
+		filter: keywords.find(k => k.keywordName === "FILTER")?.keywordValue,
+		temperature: getNumericKeyword("SET-TEMP", keywords, true) || getNumericKeyword("CCD-TEMP", keywords, true),
+		gain: getNumericKeyword("GAIN", keywords, true),
+		offset: getNumericKeyword("OFFSET", keywords, true),
+	};
+
+	const fileMetadata = {
+		exposureTime: getExposureTimeKeyword(fileName),
+		temperature: getNumericFileKeyword(fileName, "TEMP"),
+		filter: getFileKeyword(fileName, "FILTER"),
+		date: getFileKeyword(fileName, "SESSION")
+	};
+
+	const resolved = _.defaults(fileMetadata, keywordMetadata);
+	return resolved as FileMetadata;
+}
+
+function getFileKeyword(fileName: string, keywordToFind: string): string | undefined {
+	const value = new RegExp(`.*${keywordToFind}_(.*?)(?:_|.[a-zA-Z0-9]*$).*`).exec(fileName)?.[1];
+	return value;
+}
+
+function getExposureTimeKeyword(fileName: string): number | undefined {
+	const value = /.*_([0-9.]*)(?:s|_secs| s)_.*/ig.exec(fileName)?.[1];
+	return parseNumber(value, true);
+}
+
+function getNumericFileKeyword(fileName: string, keywordToFind: string): number | undefined {
+	const value = getFileKeyword(fileName, keywordToFind);
+	return parseNumber(value, true);
+}
--- src/summary/astrosummary.ts
+++ src/summary/astrosummary.ts
@@ -3,57 +3,61 @@
 import _ from "lodash";
 import fs from "fs";
 import path from "path";
-import { getNumericKeyword, readKeywords } from "../fits/keywordreader";
+import { getMetadataFromFile } from "../filemetadata";
+import { getLightFiles, getSubdirectories } from "../helpers";
 
 const dateFormat = /[0-9]{4}-[0-9]{2}-[0-9]{2}/;
 
-function getSubdirectories(path: string): string[] {
-	const subdirectories = fs.readdirSync(path, { withFileTypes: true }).filter(e => e.isDirectory());
-	return subdirectories.map(s => s.name);
-} 
+function readNotes(folder: string): string | undefined {
+	const notesPath = path.join(folder, "notes.txt");
+	const notes = fs.existsSync(notesPath) ? fs.readFileSync(notesPath, { encoding: "utf-8" }) : undefined;
+	return notes;
+}
 
 const doAsyncThings = async () => {
 	const currentWorkingDirectory = process.cwd();
-	console.log(currentWorkingDirectory);
+	// console.log(currentWorkingDirectory);
 
 	const subdirectories = getSubdirectories(currentWorkingDirectory);
 	const targets: any[] = [];
 	for(const directory of subdirectories) {
-		console.log("Target name:", directory);
+		// console.log("Target name:", directory);
 
-		const dateSubdirectories = getSubdirectories(path.join(currentWorkingDirectory, directory)).filter(s => dateFormat.test(s));
+		const targetDirectory = path.join(currentWorkingDirectory, directory);
+		const targetNotes = readNotes(targetDirectory);
+		const dateSubdirectories = getSubdirectories(targetDirectory).filter(s => dateFormat.test(s));
 		if(dateSubdirectories.length === 0) {
 			continue;
 		}
 		// console.log("Found some date directories:", dateSubdirectories.length);
-		const sessions: { date: string, exposureTime: number, sensorTemperature?: number, filter: string, count: number }[] = []; 
+		const sessions: { date: string, exposureTime: number, sensorTemperature?: number, filter: string, count: number, notes?: string }[] = []; 
 		for(const dateDirectory of dateSubdirectories) {
-			const datePath = path.join(currentWorkingDirectory, directory, dateDirectory);
-			const lightDirectory = getSubdirectories(datePath).find(s => /light[s]{0,1}/i.test(s));
-			if(!lightDirectory) {
-				continue;
-			}
-			const lightFiles =fs.readdirSync(path.join(datePath, lightDirectory), { withFileTypes: true })
-				.filter(f=> f.isFile()); 
-			const lightFile = lightFiles[0]?.name;
-			if(!lightFile) {
+			const datePath = path.join(targetDirectory, dateDirectory);
+
+			const dateNotes = readNotes(datePath);
+
+			const { lightFiles, lightDirectory } = getLightFiles(datePath);
+			const lightFile = lightFiles?.[0];
+			if(!lightFile || !lightDirectory) {
 				continue;
 			}
 
-			const keywords = readKeywords(path.join(currentWorkingDirectory, directory, dateDirectory));
-			const filter = keywords.find(k => k.keywordName === "FILTER")?.keywordValue || /.*FILTER_(.*?)(?:_|\.).*/.exec(lightFile)?.[1];
-			const keywordExposureTime = getNumericKeyword("EXPTIME", keywords, true) ;
-			const fileNameExposureTime = /.*_([0-9.]*)(?:s|_secs| s)_.*/ig.exec(lightFile)?.[1];
-			const exposureTime = keywordExposureTime || parseInt(fileNameExposureTime || "-1");
-			const sensorTemperature = getNumericKeyword("CCD-TEMP", keywords, true);
-			sessions.push({ date: dateDirectory, exposureTime, filter: filter || "None", sensorTemperature, count: lightFiles.length });
+			const metadata = getMetadataFromFile(path.join(datePath, lightDirectory, lightFile));
+			sessions.push({ 
+				date: dateDirectory, 
+				exposureTime: metadata.exposureTime || -1, 
+				filter: metadata.filter || "None", 
+				sensorTemperature: metadata.temperature, 
+				count: lightFiles.length,
+				notes: dateNotes,
+			});
 			// console.log(`${directory}--${dateDirectory}: ${exposureTime}, ${filter}`);
 		}
 
 		const groupedSessions = Object.values(_.groupBy(sessions, s => `${s.exposureTime}:${s.filter}`));
 		const summary = groupedSessions.map(group => ({ exposureTime: group[0].exposureTime, filter: group[0].filter, totalCount: _.sumBy(group, "count") }));
 
-		targets.push({ target: directory, summary, sessions });
+		targets.push({ target: directory, notes: targetNotes, summary, sessions });
 	}
 	console.log(JSON.stringify(targets));
   
