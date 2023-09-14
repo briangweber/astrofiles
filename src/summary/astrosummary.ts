55df85aa48f0910360df216ff52180d6a88aea5d
Brian Weber <brian.g.weber@gmail.com>
2023-09-13 23:51:33 -0400

    Summary: Break out different exposures/filters in the same folder into different sessions

--- src/summary/astrosummary.ts
+++ src/summary/astrosummary.ts
@@ -6,6 +6,8 @@ import path from "path";
 import { getMetadataFromFile } from "../filemetadata";
 import { getLightFiles, getSubdirectories } from "../helpers";
 
+type Session = { date: string, exposureTime: number, sensorTemperature?: number, filter: string, count: number, notes?: string };
+
 const dateFormat = /[0-9]{4}-[0-9]{2}-[0-9]{2}/;
 
 function readNotes(folder: string): string | undefined {
@@ -15,6 +17,7 @@ function readNotes(folder: string): string | undefined {
 }
 
 const doAsyncThings = async () => {
+	console.log("Running new code");
 	const currentWorkingDirectory = process.cwd();
 	// console.log(currentWorkingDirectory);
 
@@ -30,7 +33,7 @@ const doAsyncThings = async () => {
 			continue;
 		}
 		// console.log("Found some date directories:", dateSubdirectories.length);
-		const sessions: { date: string, exposureTime: number, sensorTemperature?: number, filter: string, count: number, notes?: string }[] = []; 
+		const sessions: Session[] = []; 
 		for(const dateDirectory of dateSubdirectories) {
 			const datePath = path.join(targetDirectory, dateDirectory);
 
@@ -42,16 +45,19 @@ const doAsyncThings = async () => {
 				continue;
 			}
 
-			const metadata = getMetadataFromFile(path.join(datePath, lightDirectory, lightFile));
-			sessions.push({ 
+			const fileMetadatas = lightFiles.map(f => getMetadataFromFile(path.join(datePath, lightDirectory, f)));
+			const groupedFiles = Object.values(_.groupBy(fileMetadatas, f=> `${f.exposureTime}:${f.filter}`));
+			const subsessions: Session[] = groupedFiles.map(group => ({ 
 				date: dateDirectory, 
-				exposureTime: metadata.exposureTime || -1, 
-				filter: metadata.filter || "None", 
-				sensorTemperature: metadata.temperature, 
-				count: lightFiles.length,
-				notes: dateNotes,
-			});
-			// console.log(`${directory}--${dateDirectory}: ${exposureTime}, ${filter}`);
+				exposureTime: group[0].exposureTime || -1, 
+				filter: group[0].filter || "None", 
+				sensorTemperature: group[0].temperature,
+				count: group.length,
+				notes: dateNotes 
+			}));
+			subsessions.length > 1 && console.log("Found subsessions:", targetDirectory, dateDirectory);
+
+			subsessions.length > 0 && sessions.push(...subsessions);
 		}
 
 		const groupedSessions = Object.values(_.groupBy(sessions, s => `${s.exposureTime}:${s.filter}`));
@@ -59,7 +65,12 @@ const doAsyncThings = async () => {
 
 		targets.push({ target: directory, notes: targetNotes, summary, sessions });
 	}
-	console.log(JSON.stringify(targets));
+
+	if(process.argv.includes("--csv")) {
+		console.log("You wanted a CSV. Cool story bro.");
+	} else {
+		console.log(JSON.stringify(targets));
+	}
   
 };
 
