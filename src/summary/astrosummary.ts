c24f7c8d78384e3c75c5d3959605c60cdca286d7
Brian Weber <brian.g.weber@gmail.com>
2023-09-14 00:36:05 -0400

    Add ability to generate CSV for single target

--- src/summary/astrosummary.ts
+++ src/summary/astrosummary.ts
@@ -1,11 +1,14 @@
 #!/usr/bin/env node
 
 import _ from "lodash";
+import config from "../../config";
 import fs from "fs";
 import path from "path";
 import { getMetadataFromFile } from "../filemetadata";
 import { getLightFiles, getSubdirectories } from "../helpers";
+import {stringify} from "csv-stringify/sync";
 
+type Target = { target:string; notes?: string; summary: any[]; sessions: Session[] };
 type Session = { date: string, exposureTime: number, sensorTemperature?: number, filter: string, count: number, notes?: string };
 
 const dateFormat = /[0-9]{4}-[0-9]{2}-[0-9]{2}/;
@@ -16,58 +19,80 @@ function readNotes(folder: string): string | undefined {
 	return notes;
 }
 
+function summarizeTarget(targetName: string, targetDirectory: string): Target | undefined {
+	const targetNotes = readNotes(targetDirectory);
+	const dateSubdirectories = getSubdirectories(targetDirectory).filter(s => dateFormat.test(s));
+	if(dateSubdirectories.length === 0) {
+		return undefined;
+	}
+	// console.log("Found some date directories:", dateSubdirectories.length);
+	const sessions: Session[] = []; 
+	for(const dateDirectory of dateSubdirectories) {
+		const datePath = path.join(targetDirectory, dateDirectory);
+
+		const dateNotes = readNotes(datePath);
+
+		const { lightFiles, lightDirectory } = getLightFiles(datePath);
+		const lightFile = lightFiles?.[0];
+		if(!lightFile || !lightDirectory) {
+			continue;
+		}
+
+		const fileMetadatas = lightFiles.map(f => getMetadataFromFile(path.join(datePath, lightDirectory, f)));
+		const groupedFiles = Object.values(_.groupBy(fileMetadatas, f=> `${f.exposureTime}:${f.filter}`));
+		const subsessions: Session[] = groupedFiles.map(group => ({ 
+			date: dateDirectory, 
+			exposureTime: group[0].exposureTime || -1, 
+			filter: group[0].filter || "None", 
+			sensorTemperature: group[0].temperature,
+			count: group.length,
+			notes: dateNotes 
+		}));
+		// subsessions.length > 1 && console.log("Found subsessions:", targetDirectory, dateDirectory);
+
+		subsessions.length > 0 && sessions.push(...subsessions);
+	}
+
+	const groupedSessions = Object.values(_.groupBy(sessions, s => `${s.exposureTime}:${s.filter}`));
+	const summary = groupedSessions.map(group => ({ exposureTime: group[0].exposureTime, filter: group[0].filter, totalCount: _.sumBy(group, "count") }));
+
+	return { target: targetName, notes: targetNotes, summary, sessions };
+}
+
 const doAsyncThings = async () => {
 	console.log("Running new code");
 	const currentWorkingDirectory = process.cwd();
 	// console.log(currentWorkingDirectory);
 
 	const subdirectories = getSubdirectories(currentWorkingDirectory);
-	const targets: any[] = [];
-	for(const directory of subdirectories) {
+	const targets: Target[] = [];
+
+	if(process.argv.includes("--target")) {
+		const target = summarizeTarget("Current", currentWorkingDirectory);
+		target && targets.push(target);
+	}	else {
+		for(const directory of subdirectories) {
 		// console.log("Target name:", directory);
 
-		const targetDirectory = path.join(currentWorkingDirectory, directory);
-		const targetNotes = readNotes(targetDirectory);
-		const dateSubdirectories = getSubdirectories(targetDirectory).filter(s => dateFormat.test(s));
-		if(dateSubdirectories.length === 0) {
-			continue;
+			const targetDirectory = path.join(currentWorkingDirectory, directory);
+			const target = summarizeTarget(directory, targetDirectory);
+			target && targets.push(target);
 		}
-		// console.log("Found some date directories:", dateSubdirectories.length);
-		const sessions: Session[] = []; 
-		for(const dateDirectory of dateSubdirectories) {
-			const datePath = path.join(targetDirectory, dateDirectory);
-
-			const dateNotes = readNotes(datePath);
-
-			const { lightFiles, lightDirectory } = getLightFiles(datePath);
-			const lightFile = lightFiles?.[0];
-			if(!lightFile || !lightDirectory) {
-				continue;
-			}
-
-			const fileMetadatas = lightFiles.map(f => getMetadataFromFile(path.join(datePath, lightDirectory, f)));
-			const groupedFiles = Object.values(_.groupBy(fileMetadatas, f=> `${f.exposureTime}:${f.filter}`));
-			const subsessions: Session[] = groupedFiles.map(group => ({ 
-				date: dateDirectory, 
-				exposureTime: group[0].exposureTime || -1, 
-				filter: group[0].filter || "None", 
-				sensorTemperature: group[0].temperature,
-				count: group.length,
-				notes: dateNotes 
-			}));
-			subsessions.length > 1 && console.log("Found subsessions:", targetDirectory, dateDirectory);
-
-			subsessions.length > 0 && sessions.push(...subsessions);
-		}
-
-		const groupedSessions = Object.values(_.groupBy(sessions, s => `${s.exposureTime}:${s.filter}`));
-		const summary = groupedSessions.map(group => ({ exposureTime: group[0].exposureTime, filter: group[0].filter, totalCount: _.sumBy(group, "count") }));
-
-		targets.push({ target: directory, notes: targetNotes, summary, sessions });
 	}
 
 	if(process.argv.includes("--csv")) {
-		console.log("You wanted a CSV. Cool story bro.");
+		if(!process.argv.includes("--target")) {
+			console.log("Can only export CSV on single target summaries");
+			return;
+		}
+		const mappedTargets = targets[0].sessions.map(session => ({ 
+			date: session.date, 
+			filter: config.filters[session.filter]?.astroBinFilterId, 
+			number: session.count, 
+			duration: session.exposureTime, 
+			sensorCooling: session.sensorTemperature
+		}));
+		console.log(stringify(mappedTargets, { header: true }));
 	} else {
 		console.log(JSON.stringify(targets));
 	}
