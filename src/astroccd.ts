8057d965a09b54b9fe86fd845a1358d054a31442
briangweber <briangweber@users.noreply.github.com>
2023-12-18 12:11:28 -0500

    Add ability to handle multiple different exposures, filters, etc. in a single target on a single night (#4)
    * Add darks and darkFlats for multiple exposure lengths and filters
    
    * Typescript it

---
+++ src/astroccd.ts
@@ -0,0 +1,142 @@
+#!/usr/bin/env node
+
+import fs from "fs";
+import path from "path";
+import prompt from "prompt";
+import config from "../config";
+import { FileMetadata, getMetadataFromFile } from "./filemetadata";
+import { getLightFiles } from "./helpers";
+import { tryLinkSharedCalibration, tryLinkLibraryCalibration } from "./linkcommon";
+
+const dateFormat = /[0-9]{4}-[0-9]{2}-[0-9]{2}/;
+const sessionAlreadyUpdatedFileFormat = /.*SESSION_.*/;
+const filterAlreadyUpdatedFileFormat = /.*FILTER_.*/;
+
+const currentWorkingDirectory = process.cwd();
+const currentDirectoryName = path.parse(currentWorkingDirectory).name;
+console.log(currentDirectoryName);
+
+const doTheAsyncThings = async () => {
+	const defaultCamera = Object.keys(config.cameras)[0];
+
+	const { lightFiles, lightDirectory } = getLightFiles(currentWorkingDirectory);
+
+	if(!lightDirectory) {
+		console.log("No light directory found!");
+		return;
+	}
+
+	const metadataGroups = lightFiles.reduce((prev,l) => {
+		const metadata = getMetadataFromFile(path.join(lightDirectory, l)); 
+		const key = `${metadata.exposureTime}-${metadata.filter}-${metadata.temperature}`;
+		prev[key] = prev[key] || [];
+		prev[key].push(metadata);
+		return prev;
+	}, {} as Record<string, FileMetadata[]>);
+	console.log(metadataGroups);
+	const metadata = Object.values(metadataGroups).map(value => ({ filter: value[0].filter || "", temperature: value[0].temperature, exposureTime: value[0].exposureTime, count: value.length}));
+
+	const { camera } = await prompt.get({ 
+		properties: { 
+			camera: { description: "Camera", default: defaultCamera }
+		}
+	});
+
+	if(typeof(camera) !== "string" || config.cameras[camera] === undefined) {
+		console.log("Camera does not match any known configurations:", camera);
+		return;
+	}
+
+	// We ensured we found a match for the camera above.
+	// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+	const { defaultGain, defaultOffset, filters } = config.cameras[camera]!;
+	const { gain, offset } = await prompt.get({ 
+		properties: { 
+			gain: { default: defaultGain }, 
+			offset: { default: defaultOffset}, 
+		}
+	});
+
+	// const darkFlatExposureTime = filters[filter].darkFlatDuration;
+
+	const basePath = path.join(config.sharedCalibrationRoot, camera);
+
+	// const exposureTimeString = `${exposureTime}s`;
+	const gainAndOffset = `g${gain}o${offset}`;
+	const biasFilePath = path.join("biases", `masterBias_${gainAndOffset}.xisf`);
+	// const darkFilePath = path.join("darks", `masterDark_${gainAndOffset}_${exposureTimeString}_${temperatureString}.xisf`);
+	// const darkFlatFilePath = path.join("darkFlats", `masterDarkFlat_${gainAndOffset}_${darkFlatExposureTime}s.xisf`);
+
+	const darkFilePaths: string[] = [];
+	const darkFlatFilePaths: string[] = [];
+
+	metadata.forEach(m => {
+		const exposureTimeString = `${m.exposureTime}s`;
+		const temperatureString = `${m.temperature}C`;
+		const darkFlatExposureTime = (filters as any)[m.filter].darkFlatDuration;
+		const darkFilePath = path.join("darks", `masterDark_${gainAndOffset}_${exposureTimeString}_${temperatureString}.xisf`);
+		const darkFlatFilePath = path.join("darkFlats", `masterDarkFlat_${gainAndOffset}_${darkFlatExposureTime}s.xisf`);
+
+		darkFilePaths.push(darkFilePath);
+		!darkFlatFilePaths.includes(darkFlatFilePath) && darkFlatFilePaths.push(darkFlatFilePath);
+	});
+
+	const directoriesToRename = [
+		{ original: "Bias", new: "biases", libraryFilePaths: [biasFilePath] },
+		{ original: "Light", new: "lights", dssName: "light", applyFilter: true },
+		{ original: "Dark", new: "darks", dssName: "dark", libraryFilePaths: darkFilePaths },
+		{ original: "Flat", new: "flats", dssName: "flat", applyFilter: true },
+		{ original: "DarkFlat", new: "darkFlats", libraryFilePaths: darkFlatFilePaths },
+	];
+
+	console.log(biasFilePath);
+	console.log(darkFilePaths);
+	console.log(darkFlatFilePaths);
+	console.log(metadata);
+
+	for(const directoryData of directoriesToRename) {
+		if(directoryData.original && fs.existsSync(directoryData.original)) {
+			fs.renameSync(directoryData.original, directoryData.new);
+		}
+
+		if(directoryData.original && !fs.existsSync(directoryData.new)) {
+			if(directoryData.libraryFilePaths) {
+				tryLinkLibraryCalibration(basePath, directoryData);
+			} else {
+				tryLinkSharedCalibration(currentDirectoryName, directoryData);
+			}
+		}
+
+		if(fs.existsSync(directoryData.new)) {
+			const files = fs.readdirSync(directoryData.new);
+			// let directory = `.\\${directoryData.new}`;
+			// console.log(directory);
+			for(let fileName of files) {
+				let newFileName = fileName;
+				const extension = newFileName.split(".").pop();
+
+				if(!sessionAlreadyUpdatedFileFormat.test(fileName) && dateFormat.test(currentDirectoryName)) {
+					newFileName = newFileName.replace(`.${extension}`, `_SESSION_${currentDirectoryName}.${extension}`);
+					console.log(newFileName); 
+				}
+
+				// if(directoryData.applyFilter && filter && !filterAlreadyUpdatedFileFormat.test(fileName)) {
+				// 	newFileName = newFileName.replace(`.${extension}`, `_FILTER_${filter}.${extension}`);
+				// 	console.log(newFileName);
+				// }
+
+				newFileName = newFileName.replace(/'/g, "");
+
+				if(newFileName !== fileName) {
+					const directoryOfFile = path.join(currentWorkingDirectory, directoryData.new);
+					fs.renameSync(path.join(directoryOfFile, fileName), path.join(directoryOfFile, newFileName));
+					fileName = newFileName;
+				}
+			}
+		} else {
+			console.log("No folder found for", directoryData.new);
+		}
+	}
+};
+
+doTheAsyncThings();
